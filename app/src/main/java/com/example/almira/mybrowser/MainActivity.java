0cdb629daca8a9b76f5af7ab7b36fb503e031b56
U170709005 <almiragurkan@posta.mu.edu.tr>
2020-11-06 17:31:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/almira/mybrowser/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.almira.mybrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnGo;
+    WebView webView;
+    EditText txtAddress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtAddress = findViewById(R.id.txtAddress);
+        webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+
+        if((getIntent()!= null) && (getIntent().getData()!= null)){
+            txtAddress.setText(getIntent().getData().toString());
+            webView.loadUrl(getIntent().getData().toString());
+        }
+
+
+        btnGo = findViewById(R.id.btnGo);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.loadUrl("http://" + txtAddress.getText());
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
